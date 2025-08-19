63b6cd4acf4bccbe27d7e005a4f5c06a5ad3644b
Victor Salles <vsallesa@gmail.com>
2025-08-19 00:08:51 -0300

    build: add .gitignore and remove target files

--- cap3/modulo_soma/target/debug/deps/modulo_soma-981f7b0a5e27b08d.d
+++
@@ -1,5 +0,0 @@
-/home/msratso/workspaces/BotecoDaFerramenta/desmistificando_webassembly/cap3/modulo_soma/target/debug/deps/modulo_soma-981f7b0a5e27b08d.d: src/lib.rs
-
-/home/msratso/workspaces/BotecoDaFerramenta/desmistificando_webassembly/cap3/modulo_soma/target/debug/deps/libmodulo_soma-981f7b0a5e27b08d.rmeta: src/lib.rs
-
-src/lib.rs:
